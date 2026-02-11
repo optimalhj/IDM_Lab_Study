016df4530ff66dcbf2f189ab82f54d9b66b9d94e
Desktop1 <2022112386@dgu.ac.kr>
2026-02-11 21:34:18 +0900

    Added CP_SAT scheduling code

--- Sechduling_with_CPsolver/Heuristics.py
+++ Sechduling_with_CPsolver/Heuristics.py
@@ -3,27 +3,9 @@ from ortools.sat.python import cp_model
 # ---------------------------------- Initialization / Parameter ----------------------------------------
 
 # Custom Job Set needed(same length of lists)
-Job_names = ['J1', 'J2', 'J3', 'J4', 'J5', 'J6', 'J7', 'J8', 'J9', 'J10', 'J11', 'J12', 'J13', 'J14', 'J15', 'J16', 'J17', 'J18', 'J19', 'J20',
-             'J21', 'J22', 'J23', 'J24', 'J25', 'J26', 'J27', 'J28', 'J29', 'J30', 'J31', 'J32', 'J33', 'J34', 'J35', 'J36', 'J37', 'J38', 'J39', 'J40',
-             'J41', 'J42', 'J43', 'J44', 'J45', 'J46', 'J47', 'J48', 'J49', 'J50', 'J51', 'J52', 'J53', 'J54', 'J55', 'J56', 'J57', 'J58', 'J59', 'J60',
-             'J61', 'J62', 'J63', 'J64', 'J65', 'J66', 'J67', 'J68', 'J69', 'J70', 'J71', 'J72', 'J73', 'J74', 'J75', 'J76', 'J77', 'J78', 'J79', 'J80',
-             'J81', 'J82', 'J83', 'J84', 'J85', 'J86', 'J87', 'J88', 'J89', 'J90', 'J91', 'J92', 'J93', 'J94', 'J95', 'J96', 'J97', 'J98', 'J99', 'J100']
-Process_times = [3, 4, 6, 9, 2, 3, 6, 2, 7, 7, 5, 6, 4, 8, 9, 6, 5, 7, 3, 5,
-                 9, 9, 1, 8, 2, 2, 7, 8, 9, 2, 4, 3, 8, 8, 1, 4, 9, 3, 9, 9,
-                 7, 9, 3, 4, 8, 7, 2, 5, 9, 2, 1, 8, 1, 8, 8, 6, 8, 2, 5, 2,
-                 5, 5, 2, 3, 6, 8, 2, 9, 4, 1, 6, 1, 2, 9, 1, 1, 7, 4, 5, 9,
-                 3, 5, 6, 9, 8, 1, 8, 2, 5, 3, 2, 9, 3, 3, 3, 8, 3, 1, 2, 2]
-Due_dates = [304, 143, 343, 218, 373, 260, 362, 387, 344, 204, 60, 225, 178, 324, 381, 348, 201, 337, 71, 381,
-             251, 186, 317, 202, 265, 324, 369, 10, 190, 84, 365, 46, 194, 155, 280, 14, 238, 276, 320, 9,
-             53, 376, 207, 194, 242, 217, 183, 200, 283, 230, 253, 206, 66, 197, 82, 19, 260, 283, 230, 39,
-             216, 320, 127, 63, 328, 66, 55, 304, 177, 261, 308, 61, 105, 110, 189, 278, 355, 61, 260, 308,
-             399, 336, 347, 241, 26, 292, 392, 58, 52, 2, 333, 343, 239, 179, 22, 154, 176, 161, 126, 304]
-
-plt_type = 0 # [0 : dot graph(scatter) , 1 : line graph(plot) , else -> default 1 convert]
-if plt_type != 1 or plt_type != 0:
-    plt_type = 1
-
-# ------------------------------------------------------------------------------------------------------
+Job_names = ['J1', 'J2', 'J3', 'J4', 'J5']
+Process_times = [3, 5, 2, 4, 3]
+Due_dates = [12,8,4,15,10]
 
 # Jobs to Class process / "Name" as Attribute and Randomly Building "Processing Time" and "Due Date"
 class Jobs:
@@ -36,14 +18,6 @@ class Jobs:
 def build(names, times, dues):
     new_jobs = [Jobs(names[i], times[i], dues[i]) for i in range(len(names))]
     return new_jobs
-
-# Calculating each job's tardiness and total tardiness
-def tardiness(seqs):
-    now, total_tardiness = 0, 0
-    for job in seqs:
-        now += job.processing_time
-        total_tardiness += max(now - job.due, 0)
-    return seqs, total_tardiness
 
 # ---------------------------------------------------------------------------
 
@@ -62,13 +36,11 @@ def solve_cp_sat(jobs):
     for job in jobs:
         start = md.NewIntVar(0, horizon, "start_%s"%job.name)
         end = md.NewIntVar(0, horizon, "end_%s"%job.name)
-        interval = md.NewIntervalVar(start,job.processing_time,end, "Interval_%s"%job.name)
-        td = md.NewIntVar(0, horizon, "tardiness_%s"%job.name)
 
         starts[job] = start
         ends[job] = end
-        intervals[job] = interval
-        tds[job] = td
+        intervals[job] = md.NewIntervalVar(start,job.processing_time,end, "Interval_%s"%job.name)
+        tds[job] = md.NewIntVar(0, horizon, "tardiness_%s"%job.name)
 
     md.AddNoOverlap(intervals.values())
 
@@ -96,13 +68,14 @@ if __name__ == "__main__":
         assigned_jobs = []
         starts = results["starts"]
         ends = results["ends"]
+        tds = results["tds"]
 
         for job in ini_set:
-            assigned_jobs.append({"job" : job ,"start" : starts[job], "end" : ends[job], "duration" : job.processing_time, })
-        assigned_jobs.sort(key=lambda x: x["start"])
+            assigned_jobs.append({"job" : job ,"start" : solver.value(starts[job]), "end" : solver.value(ends[job]), "duration" : job.processing_time, "td" : solver.value(tds[job])})
+        assigned_jobs.sort(key=lambda job: job["start"])
 
         for job in assigned_jobs:
-            print("start :", job["start"], "/ end :", "/ duration:", job["duration"], job["end"], "/ tardiness : %d - %d = %d" %(job["end"], job.due, job["end"] - job.due))
+            print(job["job"].name, "__ start :", job["start"], "/ duration:", job["duration"], "/ end :", job["end"], "/ tardiness : %s - %s = %s" %(job["end"], job["job"].due, job["td"]))
 
     else:
         print("No solution")
