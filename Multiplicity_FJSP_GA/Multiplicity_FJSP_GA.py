519b058a65f773cf6be92fe63fa9413cb03255aa
HPomen <kim49273557@gmail.com>
2026-04-30 20:50:43 +0900

    Completed : But need to add Graph code

--- Multiplicity_FJSP_GA/Multiplicity_FJSP_GA.py
+++ Multiplicity_FJSP_GA/Multiplicity_FJSP_GA.py
@@ -1,87 +1,6 @@
 from numpy import random as rd
 import matplotlib.pyplot as plt
 
-
-"""
-def calculate(duration, setup, operations):
-    end_machines, end_jobs = {}, {}
-    for operation in operations:
-        end_machines[operation[0]] = 0
-        end_jobs[operation[1]] = 0
-    for operation in operations:
-        start_of_operation = max(end_machines[operation[0]], end_jobs[operation[1]])
-        end_machines[operation[0]] = start_of_operation + getattr(duration, f"{operation[0]}{operation[1]}{operation[2]}")
-        end_jobs[operation[1]] = start_of_operation + getattr(duration, f"{operation[0]}{operation[1]}{operation[2]}")
-    return max(end_machines.values())
-"""
-def encode(decoded):
-    return [(gene[0], gene[2]) for gene in decoded]
-
-def generate_offsprings(duration, setup, mom, dad):
-    parents = [encode(parent[0]) for parent in [mom, dad]]
-    way_offspring = rd.choice(3)
-    i = rd.randint(2) # Main parent
-
-    if way_offspring == 0: # SPC
-        point = rd.randint(len(parents[i]))
-        offspring =
-        """
-        jobs = []
-        for  in parents[i]:
-            if operation[1] not in jobs:
-                jobs.append(operation[1])
-        fixed_job = rd.choice(jobs, size=1)
-        not_fixed_operation = [operation for operation in parents[1 - i] if operation[1] != fixed_job]
-        offspring = []
-        for idx in range(len(parents[i])):
-            if parents[i][idx][1] == fixed_job:
-                offspring.append(parents[i][idx])
-            else:
-                offspring.append(not_fixed_operation.pop(0))
-        offsprings.append(offspring)
-    """
-    elif way_offspring == 1: # UPC
-        idx1, idx2 = sorted(rd.choice([i for i in range(len(mom))], size=2, replace=False))
-        offsprings.append(
-            parents[i][0:idx1] + [(parents[1 - i][idx1][0], parents[i][idx1][1], parents[i][idx1][2])] + parents[i][
-                idx1 + 1:idx2] + [(parents[1 - i][idx2][0], parents[i][idx2][1], parents[i][idx2][2])] + parents[i][
-                idx2 + 1:])
-
-    elif way_offspring == 2:
-        idx_job = rd.randint(len(mom))
-        indices = [idx for idx in range(len(parents[i])) if parents[i][idx][1] == parents[i][idx_job][1]]
-        place = indices.index(idx_job)
-        offspring = [operation for operation in parents[i]]
-        if indices[place] != len(mom) - 1 and place != len(indices) - 1 and indices[place] + 1 != indices[
-            place + 1]:
-            offspring.insert(rd.choice(range(indices[place] + 1, indices[place + 1]), size=1)[0],
-                             offspring.pop(idx_job))
-        offsprings.append(offspring)
-
-    elif way_offspring == 3:
-        idx1, idx2 = rd.choice([i for i in range(len(mom))], size=2, replace=False)
-        offsprings.append(parents[i][0:idx1] + [
-            (rd.choice([machine for machine in m if machine != parents[i][idx1][0]]), parents[i][idx1][1],
-             parents[i][idx1][2])] + parents[i][idx1 + 1:])
-
-    elif way_offspring == 4:
-        machine_load = {machine: 0 for machine in m}
-        machine_max = m[0], 0
-        for operation in parents[i]:
-            machine_load[operation[0]] += getattr(origin, f"{operation[0]}{operation[1]}{operation[2]}")
-            if machine_load[operation[0]] > machine_max[1]:
-                machine_max = operation[0], machine_load[operation[0]]
-        machine_min = list(machine_load)[list(machine_load.values()).index(min(machine_load.values()))]
-        move_candidate = [operation for operation in parents[i] if operation[0] == machine_max[0]]
-        rand_operation = move_candidate[rd.randint(len(move_candidate))]
-        new_operation = (machine_min, rand_operation[1], rand_operation[2])
-        idx = parents[i].index(rand_operation)
-        offsprings.append(parents[i][0:idx] + [new_operation] + parents[i][idx + 1:])
-
-    else:  # Do not reach
-        offsprings = 0
-    return offsprings
-
 def select_pop(populations, way_select_pop):
     indices = [i for i in range(len(populations))]
 
@@ -89,7 +8,7 @@ def select_pop(populations, way_select_pop):
         return populations[min(rd.choice(indices, size=2, replace=False), key=lambda idx : populations[idx][1])]
 
     elif way_select_pop == 1:  # n-Size tournament
-        return populations[min(rd.choice(indices, size=rd.randint(3, max(3, int(len(indices)/2.5))), replace=False), key=lambda idx : populations[idx][1])]
+        return populations[min(rd.choice(indices, size=rd.randint(3, max(4, int(len(indices)/2.5))), replace=False), key=lambda idx : populations[idx][1])]
 
     elif way_select_pop == 2:  # Linear ranking
         return populations[rd.choice(indices, size=1, p=[2 * i / (len(indices) * (len(indices) + 1)) for i in range(1, len(indices) + 1)])[0]]
@@ -148,29 +67,99 @@ def three_phase_decode(duration, setup, chromosome, ini_set, machines):
         h += 1
     return [(seq_o[i][1][0],seq_o[i][0],seq_o[i][1][1],seq_m[i]) for i in range(len(seq_m))], max(c_slash[h-1].values())
 
+def gene_crossover(points, parent1, parent2):
+    chromosome = []
+    for gene in [parent1[idx] for idx in points]:
+        for l2 in range(len(parent2)-1, -1, -1):
+            if parent2[l2] == gene:
+                parent2.pop(l2)
+                break
+    for l in range(len(parent1)):
+        if l in points:
+            chromosome.append(parent1[l])
+        else:
+            chromosome.append(parent2.pop(0))
+    return chromosome
+
+def gene_mutation(points, chromosome):
+    insulting = [chromosome[idx] for idx in points]
+    for l in range(len(chromosome)):
+        if l in points:
+            chromosome[l] = insulting.pop(0)
+    return chromosome
+
+def encode(decoded):
+    return [(gene[0], gene[2]) for gene in decoded]
+
+def generate_offsprings(ini_set, parent1, parent2):
+    parents = [encode(parent[0]) for parent in [parent1, parent2]]
+    way_offspring = rd.choice(6)
+    if way_offspring == 0:
+        points = [i for i in range(rd.randint(1, len(parents[0])))]
+        return gene_crossover(points, parents[0], parents[1])
+
+    elif way_offspring == 1:
+        points = []
+        r_job_types = {job_type : rd.randint(len(ini_set[job_type]["jobs"])) for job_type in ini_set.keys()}
+        for l in range(len(parents[0])):
+            if r_job_types[parents[0][l][0]] > 0:
+                points.append(l)
+                r_job_types[parents[0][l][0]] -= 1
+        return gene_crossover(points, parents[0], parents[1])
+
+    elif way_offspring == 2:
+        points = []
+        rt = rd.choice(list(ini_set), size=rd.randint(1, len(list(ini_set))), replace=False)
+        for l in range(len(parents[0])):
+            job_type, op = parents[0][l]
+            if job_type in rt:
+                points.append(l)
+        return gene_crossover(points, parents[0], parents[1])
+
+    elif way_offspring == 3:
+        points = rd.choice([i for i in range(len(parents[0]))], size=rd.randint(3, len(list(parents[0]))), replace=False)
+        return gene_mutation(points, parents[0])
+
+    elif way_offspring == 4:
+        rd_type = list(ini_set)[rd.randint(len(list(ini_set)))]
+        points = [l for l in range(len(parents[0])) if rd_type == parents[0][l][0]]
+        return gene_mutation(points, parents[0])
+
+    elif way_offspring == 5:
+        rd_types = rd.choice(list(ini_set), size=rd.randint(2, len(list(ini_set))), replace=False)
+        points = [l for l in range(len(parents[0])) if parents[0][l][0] in rd_types]
+        return gene_mutation(points, parents[0])
+
+    else:  # Do not reach
+        return 0
+
 def start_ga(duration, setup, ini_set, machines, params):
-    op_types = []
-    for job_type in ini_set:
-        for _ in ini_set[job_type]["jobs"]:
-            for op in ini_set[job_type]["ops"]:
-                op_types.append((job_type, op))
-    #ini_chromosome = sorted(op_types, key=lambda op_type:getattr(duration, f"{op_type[0]}{op_type[1]}")) # by using SPT
+
     ini_pop = []
+    op_types = [(job_type, op) for job_type in ini_set for _ in ini_set[job_type]["jobs"] for op in ini_set[job_type]["ops"]]
     for _ in range(params["pop_size"]):
         chromosome = [op_types[i] for i in rd.choice(range(len(op_types)), size=len(op_types), replace=False)]
         ini_pop.append(three_phase_decode(duration, setup, chromosome, ini_set, machines))
 
+    populations = sorted(ini_pop, key=lambda case:case[1])
     for generation in range(params["num_of_gens"]):
-        ini_pop.sort(key=lambda case:case[1])
-        mating_pool = [select_pop(ini_pop, rd.randint(3)) for _ in range(params["pop_size"])]
-
+        print("Generation : ", generation + 1)
+        mating_pool = [select_pop(populations, rd.randint(3)) for _ in range(params["mating_pool"])]
         indices = [i for i in range(len(mating_pool))]
         offsprings = []
+        """
         for _ in range(len(mating_pool)//2):
             mom, dad = [indices.pop(rd.randint(len(indices))) for _ in range(2)]
+            offsprings.append(three_phase_decode(duration, setup, generate_offsprings(ini_set, populations[mom], populations[dad]), ini_set, machines))
+        """
+        for _ in range(params["num_offs"]):
+            mom, dad = rd.choice(indices, size=2, replace=False)
+            offsprings.append(three_phase_decode(duration, setup, generate_offsprings(ini_set, populations[mom], populations[dad]), ini_set, machines))
 
-            offspring = generate_offsprings(duration, setup, ini_pop[mom], ini_pop[dad])
-            offsprings.append(offspring)
+        children = sorted(populations + offsprings, key=lambda case:case[1])
+        populations = children[:params["pop_size"]]
+        print("Best offspring :", populations[0][1], populations[0][0])
+    return populations[0]
 
 class Duration:
     def __init__(self): pass
@@ -189,6 +178,7 @@ def start(durations, setups, machines_tmp, params):
                     machines.append(m)
             setattr(duration, f"{job_type}{op}", processing_time)
     machines.sort(key=lambda machine: machines_tmp.index(machine))
+
     for op_type1 in setups:
         for op_type2 in setups:
             (job_type1, op1), (job_type2, op2) = op_type1, op_type2
@@ -199,8 +189,7 @@ def start(durations, setups, machines_tmp, params):
 def main():
 
     # Parameter Input
-    params = {"pop_size": 10, "num_of_gens": 35, "ini_assign": [0.1, 0.9], "ini_seq": [0.2, 0.4, 0.4],
-              "crossover": [0.45, 0.45, 0.02, 0.02, 0.06]}
+    params = {"pop_size": 10, "num_of_gens": 15, "mating_pool": 10, "num_offs": 8}
     num_job_types ,max_num_job, max_num_op, num_machines, max_time = 5, 7, 4, 7, 9
 
     job_types = [f"Job_Type{i}" for i in range(1, num_job_types + 1)]
