6949eb68ed6e799b73e056b9c9aaec7a57d0aa7f
Hojun Kim <2022112386@dgu.ac.kr>
2026-06-27 14:16:39 +0900

    Final MU2

--- genetic_algorithm_sequence_optimizer.py
+++ genetic_algorithm_sequence_optimizer.py
@@ -4,17 +4,17 @@ import gurobipy as gp
 from gurobipy import GRB
 
 # Parameter Input
-num_jts ,max_num_job, max_num_op, num_machines, max_time = 3, 2, 3, 3, 9
-params = {"pop_size": 5, "num_of_gens": 5, "mating_pool": 20, "num_offs": 10, "o_max": 20, "s_max": 2, "T": 20, "w": 0.5, "K": 0.75}
+num_jts ,max_num_job, max_num_op, num_machines, max_time = 3, 2, 3, 5, 9
+params = {"pop_size": 5, "num_of_gens": 20, "mating_pool": 20, "num_offs": 10, "o_max": 20, "s_max": 2, "T": 20, "w": 0.5, "K": 0.25}
 
 def select_mp(populations):
     index = list(range(params["pop_size"]))
     way_pop = rd.randint(3)
 
     if way_pop == 0:  # Binary tournament
-        return populations[min(rd.choice(index, size=2, replace=False), key=lambda idx: populations[idx][1])]
+        return populations[min(rd.choice(index, size=2, replace=False), key=lambda idx: populations[idx][1][0])]
     elif way_pop == 1:  # n-Size tournament
-        return populations[min(rd.choice(index, size=rd.randint(3, max(4, int(params["pop_size"] / 2.5))), replace=False), key=lambda idx: populations[idx][1])]
+        return populations[min(rd.choice(index, size=rd.randint(3, max(4, int(params["pop_size"] / 2.5))), replace=False), key=lambda idx: populations[idx][1][0])]
     elif way_pop == 2:  # Linear ranking
         return populations[rd.choice(index, size=1, p=[2 * i / (params["pop_size"] * (params["pop_size"] + 1)) for i in range(params["pop_size"], 0, -1)])[0]]
     else:  # Do not reach
@@ -25,11 +25,11 @@ def objective(process, setup, ini_set, machines, seq):
     st_job = {jt: {job: 0 for job in ini_set[jt]["jobs"]} for jt in ini_set.keys()}
     se_setup = {}
     ops_machine, st_machine = {}, {}
+    t_setup = {}
     for m in machines:
         se_setup[m] = {}
         ops_machine[m] = []
         st_machine[m] = 0
-
     for l in range(len(seq)):
         jt, job, op, m = seq[l]
         if len(ops_machine[m]) == 0:
@@ -37,16 +37,35 @@ def objective(process, setup, ini_set, machines, seq):
         else:
             setup_time = getattr(setup, f"{ops_machine[m][0]}{ops_machine[m][1]}{jt}{op}")
             now = max(st_machine[m] + setup_time, st_job[jt][job])
+            while True:
+                pass_count = 0
+                for t in range(now - setup_time, now):
+                    if t in t_setup and t_setup[t] == params["s_max"]:
+                        now += 1
+                        break
+                    else: pass_count += 1
+                if pass_count == setup_time:
+                    break
+
             if setup_time != 0:
-                se_setup[m][f"{l-1}{l}"] = [now - setup_time, now]
+                se_setup[m][l] = [now - setup_time, now]
+                for t in range(se_setup[m][l][0], se_setup[m][l][1]):
+                    if t not in t_setup:
+                        t_setup[t] = 1
+                    else:
+                        t_setup[t] += 1
         se_process[jt][job][op].append(now)
         ops_machine[m] = (jt, op)
         st_machine[m], st_job[jt][job] = [now + getattr(process, f"{jt}{op}") for _ in range(2)]
         se_process[jt][job][op].append(st_machine[m])
+    for m in machines:
+        if len(ops_machine[m]) == 0:
+            se_setup.pop(m)
+            st_machine.pop(m)
     f_u = 0
     for jt in ini_set.keys():
-        for job in ini_set["jobs"]:
-            for op in ini_set["ops"]:
+        for job in ini_set[jt]["jobs"]:
+            for op in ini_set[jt]["ops"]:
                 if params["T"] >= se_process[jt][job][op][1]:
                     f_u += se_process[jt][job][op][1] - se_process[jt][job][op][0]
                 elif se_process[jt][job][op][0] <= params["T"] < se_process[jt][job][op][1]:
@@ -54,17 +73,18 @@ def objective(process, setup, ini_set, machines, seq):
                 else: break
     f_u /= (len([m for m in machines if len(ops_machine) != 0]) * params["T"])
     f_s = 0
-    f_c = 0
+    f_c = {}
     for m in se_setup:
+        f_c[m] = 0
         for l in se_setup[m]:
-            if params["T"] >= se_setup[1]:
+            if params["T"] >= se_setup[m][l][1]:
                 f_s += se_setup[m][l][1] - se_setup[m][l][0]
-                f_c += 1
+                f_c[m] += 1
             elif se_setup[m][l][0] <= params["T"] < se_setup[m][l][1]:
                 f_s += params["T"] - se_setup[m][l][0]
-                f_c += 1
+                f_c[m] += 1
             else: break
-    f_s /= (len([m for m in machines if len(ops_machine) != 0]) * params["T"])
+    f_s /= (len([m for m in se_setup if len(ops_machine) != 0]) * params["T"])
     f_i = 1- f_u - f_s
     f_b = max(st_machine.values()) / min(st_machine.values()) - 1
     obj = params["w"] * f_u - (1 - params["w"]) * f_s
@@ -105,7 +125,7 @@ def crossover(mating_pool, cr):
         child = crossing(points, pr1, pr2, apx=False)
     else:
         if cr == 2: # Based on F_c(# of Setup Change)
-            index.sort(key=lambda idx:mating_pool[idx][1][3])
+            index.sort(key=lambda idx:sum(mating_pool[idx][1][3].values()))
             pr1 = rd.choice(index[:int(params["mating_pool"] * params["K"])])
             pr2 = rd.choice(index[params["mating_pool"] - int(params["mating_pool"] * params["K"]):])
 
@@ -116,7 +136,8 @@ def crossover(mating_pool, cr):
                 index.sort(key=lambda idx:mating_pool[idx][1][4])
             else: pass # Do not reach
             pr1 = rd.choice(index[:int(params["mating_pool"] * params["K"])])
-            pr2 = rd.choice(index[max(params["mating_pool"] - int(params["mating_pool"] * params["K"]), pr1 + 1):])
+            if pr1 == len(index) - 1: pr2 = pr1
+            else: pr2 = rd.choice(index[max(params["mating_pool"] - int(params["mating_pool"] * params["K"]), pr1):])
         pr1, pr2 = [mating_pool[pr][0].copy() for pr in (pr1, pr2)]
         print(f"pr1 : ({len(pr1)})", pr1)
         print(f"pr2 : ({len(pr2)})", pr2)
@@ -126,40 +147,83 @@ def crossover(mating_pool, cr):
     print("-"*50)
     return child
 
-def mutation(process, setup, ini_set, machines, seq):
-    random_idx = rd.randint(len(seq))
-    jt, job, op, m = seq.pop(random_idx)
-    place_in_job = list(ini_set[jt]["ops"]).index(op)
-    if place_in_job == 0:
-        if len(list(ini_set[jt]["ops"])) > 1:
+def mutation(process, setup, ini_set, machines, seq, cr):
+    print("Mutation_chosen")
+    print(seq)
+    seq, (obj, f_u, f_s, f_c, f_i, f_b), se_process, se_setup = seq
+    seq = seq.copy()
+    if cr == 1:
+        random_idx = rd.randint(len(seq))
+        jt, job, op, m = seq.pop(random_idx)
+        place_in_job = list(ini_set[jt]["ops"]).index(op)
+        if place_in_job == 0:
+            if len(list(ini_set[jt]["ops"])) > 1:
+                for l in range(random_idx, len(seq)):
+                    if seq[l][0] == jt and seq[l][1] == job and seq[l][2] == list(ini_set[jt]["ops"])[place_in_job + 1]:
+                        next_idx = l
+                        break
+            else: next_idx = len(seq) + 1
+            if next_idx == 0: seq.insert(0, (jt, job, op, m))
+            else: seq.insert(rd.choice([i for i in range(next_idx + 1) if i != random_idx]), (jt, job, op, m))
+
+        elif place_in_job == len(ini_set[jt]["ops"]) - 1:
+            for l in range(random_idx - 1, -1, -1):
+                if seq[l][0] == jt and seq[l][1] == job and seq[l][2] == list(ini_set[jt]["ops"])[place_in_job - 1]:
+                    prior_idx = l
+                    break
+            if prior_idx == len(seq) - 1: seq.append((jt, job, op, m))
+            else: seq.insert(rd.choice([i for i in range(prior_idx + 1, len(seq) + 1) if i != random_idx]), (jt, job, op, m))
+
+        else:
+            for l in range(random_idx - 1, -1, -1):
+                if seq[l][0] == jt and seq[l][1] == job and seq[l][2] == list(ini_set[jt]["ops"])[place_in_job - 1]:
+                    prior_idx = l
+                    break
             for l in range(random_idx, len(seq)):
                 if seq[l][0] == jt and seq[l][1] == job and seq[l][2] == list(ini_set[jt]["ops"])[place_in_job + 1]:
                     next_idx = l
                     break
-        else: next_idx = len(seq) + 1
-        if next_idx == 0: seq.insert(0, (jt, job, op, m))
-        else: seq.insert(rd.choice([i for i in range(next_idx + 1) if i != random_idx]), (jt, job, op, m))
+            if next_idx - prior_idx == 1: seq.insert(next_idx, (jt, job, op, m))
+            else: seq.insert(rd.choice([i for i in range(prior_idx + 1, next_idx + 1)]), (jt, job, op, m))
+    elif cr == 2:
+        print("MU2")
+        chosen_m = max(f_c, key=lambda m : f_c[m])
+        print(se_setup[chosen_m])
+        if len(se_setup[chosen_m]) != 0:
+            chosen_idx = rd.choice(list(se_setup[chosen_m]))
+            chosen_jt, chosen_job, chosen_op, _ = seq[chosen_idx]
+            alternative_m = rd.permutation([m for m in ini_set[chosen_jt]["ops"][chosen_op] if m != chosen_m]).tolist()
+            print(f"{ini_set[chosen_jt]["ops"][chosen_op]} - {chosen_m} == {alternative_m}")
+            if len(alternative_m) != 0:
 
-    elif place_in_job == len(ini_set[jt]["ops"]) - 1:
-        for l in range(random_idx - 1, -1, -1):
-            if seq[l][0] == jt and seq[l][1] == job and seq[l][2] == list(ini_set[jt]["ops"])[place_in_job - 1]:
-                prior_idx = l
-                break
-        if prior_idx == len(seq) - 1: seq.append((jt, job, op, m))
-        else: seq.insert(rd.choice([i for i in range(prior_idx + 1, len(seq) + 1) if i != random_idx]), (jt, job, op, m))
+                for m in alternative_m:
+                    go_break = False
+                    print("Origin :", seq[chosen_idx])
+                    seq_tmp = seq.copy()
+                    seq_tmp[chosen_idx] = (chosen_jt, chosen_job, chosen_op, m)
+                    print("Change :", seq_tmp[chosen_idx], end=" --> ")
 
-    else:
-        for l in range(random_idx - 1, -1, -1):
-            if seq[l][0] == jt and seq[l][1] == job and seq[l][2] == list(ini_set[jt]["ops"])[place_in_job - 1]:
-                prior_idx = l
-                break
-        for l in range(random_idx, len(seq)):
-            if seq[l][0] == jt and seq[l][1] == job and seq[l][2] == list(ini_set[jt]["ops"])[place_in_job + 1]:
-                next_idx = l
-                break
-        if next_idx - prior_idx == 1: seq.insert(next_idx, (jt, job, op, m))
-        else: seq.insert(rd.choice([i for i in range(prior_idx + 1, next_idx + 1)]), (jt, job, op, m))
-    return objective(process, setup, ini_set, machines, seq)
+                    for prior_idx in range(chosen_idx - 1, -1, -1):
+                        if seq_tmp[prior_idx][3] == m:
+                            print(getattr(setup, f"{seq_tmp[prior_idx][0]}{seq_tmp[prior_idx][2]}{seq_tmp[chosen_idx][0]}{seq_tmp[chosen_idx][2]}"))
+                            if getattr(setup, f"{seq_tmp[prior_idx][0]}{seq_tmp[prior_idx][2]}{seq_tmp[chosen_idx][0]}{seq_tmp[chosen_idx][2]}") == 0:
+                                seq = seq_tmp.copy()
+                                go_break = True
+                            break
+                    if go_break:
+                        break
+    elif cr == 3:
+        print("MU3")
+        using_machine = []
+        for sche in seq:
+            if sche[3] not in using_machine:
+                using_machine.append(sche[3])
+        using_machine.sort(key=lambda using_m:sum(getattr(process, f"{jt}{op}") for jt, _, op, m in seq if m == using_m))
+        for using_m in sorted(using_machine):
+            print(using_m, ":", sum(getattr(process, f"{jt}{op}") for jt, _, op, m in seq if m == using_m))
+        print("Using M :", using_machine)
+    else: pass # Do not reach
+    return seq
 
 def correct_procedure(process, setup, ini_set, machines, seq):
     info_op = {jt : {job : {op : 0 for op in ini_set[jt]["ops"]} for job in ini_set[jt]["jobs"]} for jt in ini_set.keys()}
@@ -205,7 +269,7 @@ def graph_makespan(best, machines):
         ax.text(start_process + operating / 2, m, f"{jt}\n{job}\n{op}\n({operating})", va='center', ha='center', color='black', fontsize=5)
 
     for m in se_setup.keys():
-        for st_setup, ed_setup, sets in se_setup[m].values():
+        for st_setup, ed_setup in se_setup[m].values():
             setup_ing = ed_setup - st_setup
             if round(setup_ing) != 0:
                 ax.barh(m, setup_ing, left=st_setup, color='white', edgecolor='black')
@@ -221,18 +285,17 @@ def ga(process, setup, ini_set, machines):
     history = []
     ini_pop = [(jt, job, op) for jt in ini_set.keys() for job in ini_set[jt]["jobs"] for op in ini_set[jt]["ops"]]
     pops = sorted([correct_procedure(process, setup, ini_set, machines, rd.permutation(ini_pop).tolist()) for _ in range(params["pop_size"])], key=lambda case:-case[1][0])
+    print("0_result")
+    for pop in pops:
+        print(pop)
+    print("*"*50)
     cr = 1
-    for gen in range(params["num_of_gens"]):
+    for gen in range(1, params["num_of_gens"] + 1):
         mating_pool = [select_mp(pops) for _ in range(params["mating_pool"])]
         offsprings = []
-        """
-        for _ in range(len(mating_pool) // 2):
-            mom, dad = [indices.pop(rd.randint(len(indices))) for _ in range(2)]
-            pops.append(second_stage(process, setup, ini_set, machines, mating_pool[mom][0], mating_pool[dad][0]))
-        """
         for _ in range(params["num_offs"]):
-            before_mutation = crossover(mating_pool.copy(), cr)
-            offsprings.append(mutation(process, setup, ini_set, machines, before_mutation))
+            offspring = crossover(mating_pool.copy(), cr) if rd.random() < 0.5 else mutation(process, setup, ini_set, machines, mating_pool[rd.choice(list(range(params["mating_pool"])))], cr)
+            offsprings.append(objective(process, setup, ini_set, machines, offspring))
         offsprings.sort(key=lambda offs:(-offs[1][0], offs[1][1]))
 
         # seq, (md.ObjVal, f_u.X, f_s.X, f_c, f_i, f_b), se_process, se_setup
@@ -242,11 +305,12 @@ def ga(process, setup, ini_set, machines):
         print("\nOffsprings :\n")
         for values in offsprings:
             print(f"\t{values[1]}")
+
         if offsprings[0][1][0] > pops[0][1][0]:
             cr = 1
             pops = sorted(pops + offsprings, key=lambda case: (-case[1][0], -case[1][1]))[:params["pop_size"]]
             print("Replaced")
-        elif offsprings[0][1][3] > pops[0][1][3]:
+        elif sum(offsprings[0][1][3].values()) > sum(pops[0][1][3].values()):
             cr = 2
         elif offsprings[0][1][5] > pops[0][1][5]:
             cr = 3
@@ -254,7 +318,10 @@ def ga(process, setup, ini_set, machines):
             cr = 4
         else:
             cr = 1
-
+        print(f"{gen}_reuslt")
+        for pop in pops:
+            print(pop)
+        print("*"*50)
         history.append(pops[0][1][0])
 
     graph_gen(history)
