a298615d12ed0d6e1217b802d73a500d5d08592b
Hojun Kim <2022112386@dgu.ac.kr>
2026-07-01 20:44:00 +0900

    Evolution-Guided population

--- FJSP_with_DQN.py
+++ FJSP_with_DQN.py
@@ -4,11 +4,28 @@ import matplotlib.pyplot as plt
 
 # Parameter Input
 num_job, max_num_op, num_machines, max_time = 5, 5, 7, 9
-params = {"pop_size": 5, "num_of_gens": 20, "mating_pool": 20, "num_offs": 10, "s_max": 2}
+params = {"num_of_gens": 20, "mating_pool": 20, "num_offs": 10, "s_max": 2, "Np1": 10, "Np2": 20}
 
-def objective(process, setup, ini_set, seq):
+def correct_procedure(ini_set, os_vector, ms_vector):
+    seq, info_op = os_vector.copy(), {job: {op: 0 for op in ini_set[job]} for job in ini_set.keys()}
+    for l in range(len(seq)):
+        op = min(ini_set[seq[l]], key=lambda op: info_op[seq[l]][op])
+        m = rd.choice(ini_set[seq[l]][op]) if len(ms_vector) == 0 else ms_vector[l]
+        info_op[seq[l]][op] += 1
+        seq[l] = (seq[l], op, m)
+    return seq
+
+def encoding(seq):
+    os_vector, ms_vector = [], []
+    for l in range(len(seq)):
+        job, _, m = seq[l]
+        os_vector.append(job)
+        ms_vector.append(m)
+    return os_vector, ms_vector
+
+def decoding(process, setup, ini_set, vectors):
     se_process, interval = {}, {}
-
+    seq = correct_procedure(ini_set, vectors[0], vectors[1])
     horizon = num_job * max_num_op * max_time
     md = cp_model.CpModel()
 
@@ -46,30 +63,47 @@ def objective(process, setup, ini_set, seq):
     md.minimize(obj)
     solver = cp_model.CpSolver()
     solver.Solve(md)
-    return seq, solver.ObjectiveValue(), {job : {op : [solver.value(time) for time in se_process[job][op]] for op in se_process[job].keys()} for job in se_process.keys()}, {m:[(solver.value(st), solver.value(ed)) for st, ed in se_setup[m]] for m in se_setup.keys()}
+    return encoding(seq), solver.ObjectiveValue(), {job : {op : [solver.value(time) for time in se_process[job][op]] for op in se_process[job].keys()} for job in se_process.keys()}, {m:[(solver.value(st), solver.value(ed)) for st, ed in se_setup[m]] for m in se_setup.keys()}
 
-def correct_procedure(process, setup, ini_set, seq):
-    info_op = {job: {op: 0 for op in ini_set[job]} for job in ini_set.keys()}
-    for l in range(len(seq)):
-        op = min(ini_set[seq[l]], key=lambda op: info_op[seq[l]][op])
-        m = rd.choice(ini_set[seq[l]][op])
-        info_op[seq[l]][op] += 1
-        seq[l] = (seq[l], op, m)
-    return objective(process, setup, ini_set, seq)
+def evolution_guided(process, setup, ini_set, pops):
+    winner_pops, loser_pops = pops[:(params["Np1"] + 1)//2], pops[params["Np1"]//2:]
+    new_pops = []
+    for seq, _, _, _ in winner_pops:
+        os_vector, ms_vector = seq[0].copy(), seq[1].copy()
+        way = rd.randint(3)
+        if way == 0:
+            idx1, idx2 = rd.choice(range(len(os_vector)), size=2, replace=False)
+            os_vector[idx1], os_vector[idx2], ms_vector[idx1], ms_vector[idx2] = os_vector[idx2], os_vector[idx1], ms_vector[idx2], ms_vector[idx1]
+        elif way == 1:
+            idx1, idx2 = sorted(rd.choice(range(len(os_vector)), size=2, replace=False))
+            if idx1 != 0: os_vector[idx1:idx2+1], ms_vector[idx1:idx2+1] = os_vector[idx2:idx1-1:-1], ms_vector[idx2:idx1-1:-1]
+            else: os_vector[idx1:idx2 + 1], ms_vector[idx1:idx2 + 1] = os_vector[idx2::-1], ms_vector[idx2::-1]
+        elif way == 2:
+            idx = rd.choice(range(len(os_vector)))
+            job, op, m_tmp = correct_procedure(ini_set, os_vector, ms_vector)[idx]
+            ms_vector[idx] = rd.choice([m for m in ini_set[job][op] if m != m_tmp]) if len(ini_set[job][op]) != 1 else m_tmp
+        else: os_vector, ms_vector = [], [] # Do not reach
+        new_pops.append(decoding(process, setup, ini_set, (os_vector, ms_vector)))
+    for _ in range(len(winner_pops)):
+        pr1, pr2 = rd.permutation([winner_pops.pop(rd.randint(len(winner_pops)))[0], loser_pops.pop(rd.randint(len(loser_pops)))[0]]).tolist()
+        os_vector = pr1[0]
+        pr1, pr2 = correct_procedure(ini_set, pr1[0], pr1[1]), correct_procedure(ini_set, pr2[0], pr2[1])
+        ms_vector = []
+        for job, op, _ in pr1:
+            for job2, op2, m in pr2:
+                if job == job2 and op == op2:
+                    ms_vector.append(m)
+                    break
+        new_pops.append(decoding(process, setup, ini_set, (os_vector, ms_vector)))
+    return new_pops
 
-def graph_makespan(best, machines):
+def knowledge_drived(process, setup, ini_set, pops):
+    seq = []
+    return decoding(process, setup, ini_set, seq)
+
+def graph_makespan(ini_set, machines, best):
     seq, obj, se_process, se_setup = best
-    print(seq)
-    print("------")
-    for job in se_process.keys():
-        for op in se_process[job].keys():
-            print(f"({job}, {op}) : {se_process[job][op]}")
-    print("------")
-    for m in machines:
-        if m in se_setup:
-            print(m)
-            for idx in se_setup[m]:
-                print("\t",idx)
+    seq = correct_procedure(ini_set, seq[0], seq[1])
 
     _, ax = plt.subplots()
     s_job, s_machine, m_tmp = list(se_process.keys()), {}, {}
@@ -99,12 +133,27 @@ def graph_makespan(best, machines):
     plt.show()
 
 def cp_aea(process, setup, ini_set, machines):
-    ini_pop = [job for job in ini_set.keys() for _ in range(len(ini_set[job]))]
-    pops = sorted([correct_procedure(process, setup, ini_set, rd.permutation(ini_pop).tolist()) for _ in range(params["pop_size"])], key=lambda case: case[1])
+    ini_os_vector = [job for job in ini_set.keys() for _ in range(len(ini_set[job]))]
+    pops = sorted([decoding(process, setup, ini_set, (rd.permutation(ini_os_vector).tolist(), ())) for _ in range(params["Np1"] + params["Np2"])], key=lambda case: case[1])
+    print("Gen 0", "="*30)
+    print("Ini Pop :")
+    for pop in pops:
+        print("\t", pop[1], pop)
+    for gen in range(1, params["num_of_gens"] + 1):
+        print(f"Gen {gen}", "=" * 30)
+        print("Pops :")
+        for pop in pops:
+            print("\t", pop[1], pop)
+        egp, kdp = pops[:params["Np1"]], pops[:params["Np2"]]
+        offsprings = sorted(evolution_guided(process, setup, ini_set, egp), key=lambda case: case[1])
+        print("Offspring :")
+        for off in offsprings:
+            print("\t", off[1], off)
+        pops = sorted(pops + offsprings, key=lambda case: case[1])[:params["Np1"]]
+        print()
 
 
-
-    graph_makespan(pops[0], machines)
+    graph_makespan(ini_set, machines, pops[0])
     return 0
 
 class Process:
@@ -130,6 +179,7 @@ def start(processes, setups, machines_tmp):
             setattr(setup, f"{job1}{op1}{job2}{op2}", setups[(job1, op1)][(job2, op2)])
 
     cp_aea(process, setup, ini_set, machines)
+
 def main():
 
     machines = [f"M{i}" for i in range(1, num_machines + 1)]
