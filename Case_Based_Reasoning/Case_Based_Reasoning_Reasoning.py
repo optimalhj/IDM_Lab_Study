075823751253237106c178e25c7d6501f2e83872
Hojun Kim <2022112386@dgu.ac.kr>
2026-07-12 16:34:40 +0900

    Final Commit

--- Case_Based_Reasoning/Case_Based_Reasoning_Reasoning.py
+++ Case_Based_Reasoning/Case_Based_Reasoning_Reasoning.py
@@ -1,11 +1,8 @@
 import mysql.connector
 from Case_Based_Reasoning_CaseGeneration_Initialization import user, password, db_name, case_database
-from Case_Based_Reasoning_CaseGeneration_Repetition import params, max_num_job, correct_procedure, max_time
+from Case_Based_Reasoning_CaseGeneration_Repetition import params, max_num_job, max_time
 from numpy import random as rd
 import matplotlib.pyplot as plt
-
-def graph_makespan(process, setup, ini_set, seqs, machines):
-    print(seqs)
 
 def str_to_list(strings, forward_add = "", backward_add = ""):
     to_list = []
@@ -23,14 +20,11 @@ def start(process, setup, p_prime, ini_set,  machines):
     conn = mysql.connector.connect(user=user, password=password, database=db_name)
     cursor = conn.cursor()
     cursor.execute(f"SELECT COUNT(*) FROM {case_database};")
-    N_R = 100
-    H = list(range(1, [i[0] for i in cursor][0] + 1))
-    R = []
+    N_R, H ,R = 10, list(range(1, [i[0] for i in cursor][0] + 1)), []
     sigma = N_R - len(H)
 
     cursor.execute(f"SELECT * FROM {case_database} ORDER BY {params["w"]} * alt + ({1 - params["w"]}) * awt ASC;")
-    database = [row for row in cursor]
-    for c in database:
+    for c in [row for row in cursor]:
         while len(R) < N_R:
             h, _, _, _= c
             if h in H:
@@ -40,6 +34,7 @@ def start(process, setup, p_prime, ini_set,  machines):
                 if c not in R: R.append(c)
                 sigma -= 1
             else: break
+
     history = []
     for c in R:
         cursor.execute(f"SELECT job_type, op FROM s_o_{c[0]} ORDER BY info ASC")
@@ -47,40 +42,50 @@ def start(process, setup, p_prime, ini_set,  machines):
         dij = {jt : {op : p_prime[jt] - p[jt] for op in ini_set[jt]["ops"].keys()} for jt in ini_set.keys()}
         for u in range(1, len(s_o) + 1):
             jt, op = s_o[len(s_o) - u]
-            if dij[jt][op] < 0:
-                dij[jt][op] += 1
+            if dij[jt][op] < 0: dij[jt][op] += 1
             else:
                 s_o_prime.insert(0, (jt, op))
                 while dij[jt][op] > 0:
                     s_o_prime.insert(0, (jt, op))
                     dij[jt][op] -= 1
-        delta = {jt : {job : 0 for job in ini_set[jt]["jobs"]} for jt in ini_set.keys()}
-        another_set = {jt : {op : [job for job in ini_set[jt]["jobs"]] for op in ini_set[jt]["ops"]} for jt in ini_set.keys()}
-        f = {m : [0, ()] for m in machines}
-        new_seq = []
+
+        _, ax = plt.subplots()
+        f, delta = {}, {(jt, job) : 0 for jt in ini_set.keys() for job in ini_set[jt]["jobs"]}
+        for m in machines:
+            f[m] = [0, ()]
+            ax.barh(m, 0, left = 0)
+
+        another_set, new_seq = {jt : {op : ini_set[jt]["jobs"].copy() for op in ini_set[jt]["ops"]} for jt in ini_set.keys()}, []
         for u in range(1, len(s_o_prime) + 1):
             jt, op = s_o_prime[u - 1]
             job = another_set[jt][op].pop(0)
-            horizon = max_time * len(s_o_prime)
-            v, o_star, m_star = {}, "o", "m"
+            end, v, m_star, duration = max_time * len(s_o_prime), {}, "m", getattr(process, f"{jt}{op}")
             for m in ini_set[jt]["ops"][op]:
-                if len(f[m][1]) == 0:
-                    v[m] = getattr(process, f"{jt}{op}")
-                else:
-                    prior_jt, prior_op = f[m][1]
-                    v[m] = getattr(process, f"{jt}{op}") + getattr(setup, f"{prior_jt}{prior_op}{jt}{op}") + max(f[m][0], delta[jt][job])
-                if horizon > v[m]:
-                    o_star, m_star, horizon = (jt, job, op), m, v[m]
+                v[m] = duration + getattr(setup, f"{f[m][1][0]}{f[m][1][1]}{jt}{op}") + max(f[m][0], delta[(jt, job)]) if len(f[m][1]) else duration + max(f[m][0], delta[(jt, job)])
+                if end > v[m]: m_star, end = m, v[m]
 
-            f[m_star] = [horizon, (jt, op)]
-            delta[jt][job] = horizon
+            ax.barh(m_star, duration, left=end - duration, color=plt.get_cmap('tab20', len(delta))(list(delta).index((jt, job))), edgecolor='black')
+            ax.text(end - duration / 2, m_star, f"{jt}\n{job}\n{op}\n({duration})", va='center', ha='center', color='black', fontsize=7)
 
-            new_seq.append((o_star[0], o_star[1], o_star[2], m_star))
-        history.append([new_seq, max(f[m][0] for m in machines)])
+            setup_time = getattr(setup, f"{f[m_star][1][0]}{f[m_star][1][1]}{jt}{op}") if len(f[m_star][1]) else 0
+            if len(f[m_star][1]) and setup_time:
+                ax.barh(m_star, setup_time, left=end - duration - setup_time, color='white', edgecolor='black')
+                ax.text(end - duration - setup_time/2, m_star, f"({setup_time})", va='center', ha='center', color='black', fontsize=7)
+
+            f[m_star], delta[(jt, job)] = [end, (jt, op)], end
+            new_seq.append((jt, job, op, m_star))
+        history.append([new_seq, max(f[m][0] for m in machines), len(history) + 1])
+
+        ax.set_xticks([i for i in range(int(history[-1][1]) + 2)])
+        ax.tick_params(axis='x', labelsize=5)
+        ax.set_yticks(range(len(machines)))
+        ax.set_yticklabels(machines)
+        ax.set_xlabel("Time")
+        ax.set_title(f"Makespan_Result({history[-1][2]})")
+        plt.show()
     conn.close()
     history.sort(key=lambda case:case[1])
-
-    graph_makespan(process, setup, ini_set, history[0], machines)
+    print(f"({history[0][2]}) -> Makespan : {history[0][1]} / Sequence : {history[0][0]}")
 
 class Duration:
     def __init__(self): pass
@@ -91,14 +96,16 @@ def main():
     process, setup, ini_set = Duration(), SetUp(), {}
     conn = mysql.connector.connect(user=user, password=password, database=db_name)
     cursor = conn.cursor()
+
     cursor.execute(f"SELECT job_type, op, process, machines FROM PROCESSES")
     for jt, op, process1, ms in cursor:
         setattr(process, f"{jt}{op}", process1)
-        if jt not in ini_set: ini_set[jt] = {"ops" : {}, "jobs" : [f"Job{i}" for i in range(1, rd.randint(2, max_num_job + 1))] }
+        if jt not in ini_set: ini_set[jt] = {"ops" : {}, "jobs" : [f"Job{i}" for i in range(1, rd.randint(2, max_num_job + 1))]}
         ini_set[jt]["ops"][op] = str_to_list(ms, forward_add="M")
+
     cursor.execute(f"SELECT PRIOR_JT, PRIOR_OP, NOW_JT, NOW_OP, SETUP FROM SETUPS")
-    for jt1, op1, jt2, op2, setup1 in [row for row in cursor]:
-        setattr(setup, f"{jt1}{op1}{jt2}{op2}", setup1)
+    for jt1, op1, jt2, op2, setup1 in [row for row in cursor]: setattr(setup, f"{jt1}{op1}{jt2}{op2}", setup1)
+
     cursor.execute(f"SELECT M FROM MACHINES")
     machines = [m[0] for m in cursor]
     conn.close()
@@ -107,10 +114,7 @@ def main():
         for op in ini_set[jt]["ops"]:
             print("\t", op, ini_set[jt]["ops"][op])
     print("-"*50)
-    p_prime = {jt:len(ini_set[jt]["jobs"]) for jt in ini_set.keys()}
-    print(p_prime)
-    start(process, setup, p_prime, ini_set, machines)
-
+    start(process, setup, {jt:len(ini_set[jt]["jobs"]) for jt in ini_set.keys()}, ini_set, machines)
 
 if __name__=="__main__":
     main()
